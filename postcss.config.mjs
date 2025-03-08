71e66ab3d3f2e392f3dbe3b18fefc0ef52a5dada
peanut996 <peanut996@icloud.com>
2025-03-08 20:25:47 +0800

    build successs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
+
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
-
 export default config;
